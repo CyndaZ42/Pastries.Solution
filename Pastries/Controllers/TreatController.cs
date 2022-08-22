d1ae9294e34998d8a5f8bd693bd2042d3338b808
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-08-21 19:07:28 -0700

    Add Many to Many Functionality

--- Pastries/Controllers/TreatController.cs
+++ Pastries/Controllers/TreatController.cs
@@ -42,6 +42,7 @@ namespace Pastries.Controllers
         .FirstOrDefault(treat => treat.TreatId == id);
         return View(thisTreat);
     }
+    
     public ActionResult Edit(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
@@ -60,6 +61,24 @@ namespace Pastries.Controllers
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
+    }
+
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
     }
 
     [HttpPost, ActionName("Delete")]
